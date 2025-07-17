34dbe05c2947813fdc241310a340142d079f2e38
Andrew Sears <andrew@mayfifteenth.com>
2025-07-17 12:02:24 -0500

    Add plugin service

---
+++ app/Providers/PluginServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PluginServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->loadPlugins();
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->bootPlugins();
+    }
+
+    protected function loadPlugins(): void
+    {
+        $pluginsPath = app_path('Plugins');
+        foreach (glob($pluginsPath . '/*') as $plugin) {
+            if (is_dir($plugin)) {
+                $pluginClass = 'App\Plugins\\' . basename($plugin) . '\\' . basename($plugin) . 'Plugin';
+                if (class_exists($pluginClass)) {
+                    $pluginInstance = new $pluginClass();
+                    $pluginInstance->register();
+                }
+            }
+        }
+    }
+
+    protected function bootPlugins(): void
+    {
+        $pluginsPath = app_path('Plugins');
+        foreach (glob($pluginsPath . '/*') as $plugin) {
+            if (is_dir($plugin)) {
+                $pluginClass = 'App\Plugins\\' . basename($plugin) . '\\' . basename($plugin) . 'Plugin';
+                if (class_exists($pluginClass)) {
+                    $pluginInstance = new $pluginClass();
+                    $pluginInstance->boot();
+                }
+            }
+        }
+    }
+}
